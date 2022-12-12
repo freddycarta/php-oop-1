9d713354896e3f27ac7cf07fc2dd5d38bc41069a
freddycarta <cartafederico92@gmail.com>
2022-12-12 18:03:24 +0100

    commit

--- index.php
+++ index.php
@@ -4,14 +4,16 @@ include_once __DIR__ . '/models/movie.php';
 
 
 $pirates = new movie();
-$pirates->name = "pirates"; 
+$pirates->title = "pirates"; 
 $pirates->date = "23/23/23"; 
+$pirates->genre = "action";
 
 var_dump($pirates);
 
 $vampires = new movie();
-$pirates->name = "vampires"; 
+$pirates->title = "vampires"; 
 $pirates->date = "23/11/23";
+$pirates->genre = "horror";
 
 var_dump($vampires)
 
